2a712033c67a7868429a3b190ab46efc43529e8e
Utku Karatas <utku.karatas3@gmail.com>
2024-07-20 20:41:43 +0300

    welcome page created

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Oyun ve Uygulama Akademisi Flutter Grup 30
 [Sprint 1](sprint_files/sprint_1/sprint_1.md)
+
+# Ekran Görüntüleri
+![screenshot_1](screenshots/Screenshot_1721497053.png)
---
+++ lib/screens/welcome_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    Color orangeColor = const Color.fromARGB(255, 254, 165, 1100);
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: screenHeight / 2,
+              decoration: BoxDecoration(
+                color: orangeColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.elliptical(200, 100),
+                  bottomRight: Radius.elliptical(200, 100),
+                ),
+              ),
+              child: Stack(
+                clipBehavior: Clip.none, // Allows overflow
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "PETPET",
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 36,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -screenHeight /
+                        11, // Adjust this value to control how much of the image overflows
+                    left: 0,
+                    right: 0,
+                    child: Image.asset("assets/images/petpet_cat_1.png"),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.black, // Default color for the text
+                  ),
+                  children: [
+                    const TextSpan(text: "En yakın "),
+                    TextSpan(
+                      text: "dostlarınızı",
+                      style: TextStyle(color: orangeColor), // Highlight color
+                    ),
+                    const TextSpan(text: " güvenle takip edin"),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                "Her anlarını kaydedin ve diğer hayvanseverlerle iletişime geçin.",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    color: const Color.fromARGB(255, 148, 148, 148)),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(backgroundColor: orangeColor),
+                child: Text(
+                  "Devam Et",
+                  style: GoogleFonts.poppins(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
